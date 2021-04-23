c5a2299062f438a40e51d40aece3c14e89bcfca5
sbb777 <oct13y02@naver.com>
2021-04-24 08:53:38 +0900

    test.js

---
+++ scratch/test.js
@@ -0,0 +1,24 @@
+class Test {
+
+    constructor() {}
+  
+    getInfo() { // 拡張機能の各種情報
+      return {
+        id: 'test',
+        name: 'Test', // 拡張機能の名前
+        blocks: [ // 各ブロックの定義
+          {
+            opcode: 'hello', // このブロックが実行されると、helloという関数が呼ばれる
+            blockType: Scratch.BlockType.COMMAND,　// 「10歩動かす」のような通常の命令ブロック
+            text: 'hello' // ブロックに表示されるテキスト
+          }
+        ]
+      }
+    }
+  
+    hello() {
+      console.log('hello'); // console log に hello と出力
+    }
+  }
+  
+  Scratch.extensions.register(new Test());
