a7a21afeca8c598d1398f1cca71a88b7b0440669
sbb777 <oct13y02@naver.com>
2021-04-24 09:17:33 +0900

    test1

---
+++ saved/test1.js
@@ -0,0 +1,24 @@
+class Test {
+
+    constructor () {}
+ 
+    getInfo () {// 확장의 각종 정보
+      return {
+        id : 'test'
+        name : 'Test', // 확장의 이름
+        blocks : // 각 블록의 정의
+          {
+            opcode : 'hello', //이 블록이 실행되면 hello라는 함수가 호출되는
+            blockType : Scratch.BlockType.COMMAND // "10 걸음 이동"과 같은 일반적인 명령 블록
+            text : 'hello'// 블록에 표시되는 텍스트
+          }
+        ]
+      }
+    }
+ 
+    hello () {
+      console.log ( 'hello'); // console log에 hello 출력
+    }
+  }
+ 
+  Scratch.extensions.register (new Test ());
